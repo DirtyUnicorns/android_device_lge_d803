a66fe2eaec0c4324a2ea45edfe642388569e588a
Jake Whatley <jacob.whatley91@gmail.com>
2014-10-12 16:39:39 -0400

    update d803
    Change-Id: I208b1d0197ea594adcb6833bb127ae330d382998

--- omni_d803.mk
+++ omni_d803.mk
@@ -1,5 +1,4 @@
-#
-# Copyright (C) 2011 The Android Open-Source Project
+# Copyright (C) 2012 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,22 +11,38 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+
+# Inherit APNs list
+$(call inherit-product, vendor/du/config/gsm.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/du/config/common.mk)
 
 $(call inherit-product-if-exists, vendor/lge/galbi/galbi-gsm-vendor.mk)
 $(call inherit-product, device/lge/g2-common/g2.mk)
+
+# Discard inherited values and use our own instead.
+PRODUCT_NAME := du_d803
+PRODUCT_DEVICE := d803
+PRODUCT_BRAND := lge
+PRODUCT_MANUFACTURER := lge
+PRODUCT_MODEL := LG-D803
 
 ## overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 PRODUCT_PROPERTY_OVERRIDES += \
-	telephony.lteOnGsmDevice=1 \
-	ro.telephony.default_network=9
+        telephony.lteOnGsmDevice=1 \
+        ro.telephony.default_network=9
 
 PRODUCT_COPY_FILES += \
-	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+        frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
 
 # NFC packages
 PRODUCT_PACKAGES += \
     nfc_nci.g2 \
     NfcNci
+
